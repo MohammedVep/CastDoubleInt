444dd1bbd3e6acc40f3afd473d6df4a9c8a906ee
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2024-06-27 16:19:42 -0400

    Completed Cast a Double to an Int exercise

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double number = 859.46;
+        int result = (int) number;
+        System.out.println("The result is " + result);
+    }
+}
